aef684f1f2da5a24f652274823540b2cbe7fbd21
ArthurCavalari <160640914+ArthurCavalari@users.noreply.github.com>
2024-04-30 09:53:33 -0300

    Add files via upload

---
+++ codigosJava/Herança/PessoaFisica.java
@@ -0,0 +1,9 @@
+public class PessoaFisica extends Pessoa {
+    private String rg;
+    public void setRg(STring rg) {
+        this.rg = rg;
+    }
+    public String getRg(){
+        return (rg);
+    }
+}
